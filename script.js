c6c4494f66387004466a7fbb8e3af429c49c3d62
Isaac Saxonov <isaac.saxonov@gmail.com>
2023-02-13 14:01:12 -0800

    fixed transition

--- script.js
+++ script.js
@@ -19,15 +19,31 @@ $(document).ready(function(){
                 $("#secondscreen").removeClass("fixed");
             }
         }
-    
-        /*if (pos > winH * 2 && pos < winH * 3){
-            if(lastScrollTop > lastScrollTop){
-                pos
-                $('changertextone').css("opacity",)
-            }
-            
+        if(pos < winH * 3 ){
+            $('changertextone').css("opacity", "0");
+            $('changertexttwo').css("opacity", "0");
         }
-        lastScrollTop = pos;*/
+    
+        if (pos > winH * 3 && pos < winH * 4){
+
+                
+                var addition = 75 - 75 * (pos - winH * 3)/(winH * 4 - winH * 3);
+                var subtraction = -75 * (pos - winH * 3)/(winH * 4 - winH * 3);
+                var opacitysubtraction = 1 - (pos - winH * 3)/(winH * 4 - winH * 3);
+                var opacityaddition = (pos - winH * 3)/(winH * 4 - winH * 3);
+                console.log(addition);
+                
+                $('#changertextone').css("opacity", opacitysubtraction);
+                $('#changertexttwo').css("opacity", opacityaddition);
+                $('#changertextone').css({"transform" : "translateY(" + subtraction + "%)"});
+                $('#changertexttwo').css({"transform" : "translateY(" + addition + "%)"});
+
+            
+        }else if(pos > winH*4){
+            $('changertextone').css("opacity", "0");
+        }
+        
+        lastScrollTop = pos;
     
     });
 
