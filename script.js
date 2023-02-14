72ce45627fc18705de95e404f71123797aa6e745
Isaac Saxonov <isaac.saxonov@gmail.com>
2023-02-13 22:11:02 -0800

    added

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
         var pos = $(window).scrollTop();
 
         if(pos > winH){
+            console.log("weirdo")
             if(!($("#secondscreen").hasClass("fixed"))){
                 $("#secondscreen").addClass("fixed");
             }
@@ -19,28 +20,33 @@ $(document).ready(function(){
                 $("#secondscreen").removeClass("fixed");
             }
         }
-        if(pos < winH * 3 ){
-            $('changertextone').css("opacity", "0");
-            $('changertexttwo').css("opacity", "0");
+        if(pos < winH * 2 ){
+            
+            
+            $('#changertexttwo').css("opacity", "0");
         }
     
-        if (pos > winH * 3 && pos < winH * 4){
+        if (pos > winH * 2 && pos < winH * 3){
+            
 
                 
-                var addition = 75 - 75 * (pos - winH * 3)/(winH * 4 - winH * 3);
-                var subtraction = -75 * (pos - winH * 3)/(winH * 4 - winH * 3);
-                var opacitysubtraction = 1 - (pos - winH * 3)/(winH * 4 - winH * 3);
-                var opacityaddition = (pos - winH * 3)/(winH * 4 - winH * 3);
+                var addition = 300 - 300 * (pos - winH * 2)/(winH * 3 - winH * 2);
+                var subtraction = -300 * (pos - winH * 2)/(winH * 3 - winH * 2);
+                var opacitysubtraction = 1 - (pos - winH * 2)/(winH * 3 - winH * 2);
+                var opacityaddition = (pos - winH * 2)/(winH * 3 - winH * 2);
                 console.log(addition);
                 
                 $('#changertextone').css("opacity", opacitysubtraction);
                 $('#changertexttwo').css("opacity", opacityaddition);
-                $('#changertextone').css({"transform" : "translateY(" + subtraction + "%)"});
-                $('#changertexttwo').css({"transform" : "translateY(" + addition + "%)"});
+                $('#changertextone').css({"transform" : "translateY(" + subtraction + "px)"});
+                $('#changertexttwo').css({"transform" : "translateY(" + addition + "px)"});
 
             
-        }else if(pos > winH*4){
-            $('changertextone').css("opacity", "0");
+        }
+        if(pos > winH*3){
+            
+            $('#changertextone').css("opacity", "0");
+            
         }
         
         lastScrollTop = pos;
